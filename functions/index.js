6b734fc987d4de7b478c3e2135acee95f4da8f14
Juan Carlos M <Juan Carlos M>
2019-09-17 21:34:41 -0500

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express();
+app.get('/timestamp',(request, response)=>{
+    response.send(`${Date.now()}`);
+    console.log("hola");
+    
+});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.app = functions.https.onRequest(app);
